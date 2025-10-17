aa1ae5048cb97fdfeb3cfe508df13771abde18ab
SR <saodat.riskiyeva@gmail.com>
2025-10-16 22:13:49 -0400

    enhancing the API

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 
+const db = require("./data/database");
+const quoteRoutes = require("./routes/quotes.routes");
+
 const app = express();
 
-app.get("/quote", function (req, res, next) {
-  res.json({
-    quote:
-      "As You dive deeper into Web Development, Web Development will dive deeper into You!",
+app.use("/quote", quoteRoutes);
+
+app.use(function (error, req, res, next) {
+  res.status(500).json({
+    message: "Something went wrong!",
   });
 });
 
-app.listen(3000);
+db.initDb()
+  .then(function () {
+    app.listen(3000);
+  })
+  .catch(function (error) {
+    console.log("Connecting to the database failed!");
+  });
